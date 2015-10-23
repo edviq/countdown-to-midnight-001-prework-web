3fbd65faf65680c4e894207a08ad0727a57480b0
edviq <elukyanov@gmail.com>
2015-10-23 17:06:43 -0400

    Countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(x)
 
-def countdown
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
+
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+x>=5
+sleep(5)
+
 end
